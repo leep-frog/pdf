c94715b196aefff454a5248cbd72a59fe10cc351
leep-frog <gleeper49@gmail.com>
2021-11-01 08:11:08 -0400

    change

--- pdf.go
+++ pdf.go
@@ -211,10 +211,12 @@ func (pdf *PDF) Crop(width, height float64, inputPath string, outputPath string)
 			}
 
 			// Crop from top left corner, so we only change lower left y (lly) and upper right x (urx).
-			//(*bbox).Lly = height
+			//(*bbox).Ury = height
 			//(*bbox).Urx = width
 			fmt.Println((*bbox), (*bbox).Llx, (*bbox).Llx, (*bbox).Urx, (*bbox).Ury)
 			fmt.Println(height, width)
+			(*bbox).Llx = 300
+			(*bbox).Lly = 300
 
 			/*// Zoom in on the page middle, with a scaled width and height.
 			width := (*bbox).Urx - (*bbox).Llx
